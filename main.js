be94f335cdc7e239eed1e5b6b1183dddc04bd7cd
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2020-04-04 00:51:21 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '');
+ 
+game.state.add('play', {
+    preload: function() {
+    },
+    create: function() {
+        this.gameData = {
+            gold: 0,
+            goldperclick: 1,
+            goldpersecond: 0,
+            miners: 0,
+            minerProduction: 0.1,
+            minerCost: 10
+        };
+    },
+    render: function() {
+        game.debug.text('Welcome to the mimes, Gold Miner!', 250, 290);
+    }
+});
+ 
+game.state.start('play');
